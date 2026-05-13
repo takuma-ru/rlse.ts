9f43be2e3f4737b65d028c6fec9086989cb6c6fc
takuma-ru <kanekotakuma@team-lab.com>
2026-05-13 11:10:41 +0900

    Update footer copyright year to 2026

--- docs/src/components/layouts/Footer/Footer.tsx
+++ docs/src/components/layouts/Footer/Footer.tsx
@@ -1,5 +1,5 @@
 import { footer } from "./Footer.css";
 
 export const Footer = () => {
-  return <footer className={footer}>&copy; takuma-ru 2024-</footer>;
+  return <footer className={footer}>&copy; takuma-ru 2026-</footer>;
 };
