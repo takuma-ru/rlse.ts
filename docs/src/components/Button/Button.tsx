77f92bcfbb2687306723c77ff396a21e3aa109ba
takuma-ru <kanekotakuma@team-lab.com>
2026-05-13 10:07:40 +0900

    Remove clsx dependency and update Button and Code components to use cx for className handling

--- docs/src/components/Button/Button.tsx
+++ docs/src/components/Button/Button.tsx
@@ -1,7 +1,7 @@
-import clsx from "clsx";
 import type { Child } from "hono/jsx";
 import type { JSX } from "hono/jsx";
 import { anchor, button } from "./Button.css";
+import { cx } from "hono/css";
 
 type Props = { children?: Child } & (AnchorProps | ButtonProps);
 type AnchorProps = { tagType: "a" } & JSX.IntrinsicElements["a"];
@@ -15,7 +15,7 @@ export const Button = (props: Props) => {
       const { children, ...attr } = props;
 
       return (
-        <a {...attr} className={clsx(anchor, attr.className)}>
+        <a {...attr} className={cx(anchor, attr.className)}>
           {children}
         </a>
       );
@@ -24,7 +24,7 @@ export const Button = (props: Props) => {
       const { children, ...attr } = props;
 
       return (
-        <button {...attr} className={clsx(button, attr.className)}>
+        <button {...attr} className={cx(button, attr.className)}>
           {children}
         </button>
       );
--- docs/src/components/mdx/Code/Code.tsx
+++ docs/src/components/mdx/Code/Code.tsx
@@ -1,6 +1,6 @@
-import clsx from "clsx";
 import type { JSX } from "hono/jsx";
 import { inlineCode } from "./Code.css";
+import { cx } from "hono/css";
 
 type Props = JSX.IntrinsicElements["code"];
 
@@ -8,7 +8,7 @@ export const Code = ({ children, ...attr }: Props) => {
   return (
     <code
       {...attr}
-      className={clsx(attr.lang === "inline" && inlineCode, attr.className)}
+      className={cx(attr.lang === "inline" && inlineCode, attr.className)}
     >
       {children}
     </code>
